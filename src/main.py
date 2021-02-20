73a6790c408f72cbd8ffb80a97905dc9a2eb2ba4
Saaketh Vangati <saaketh.vangati@gmail.com>
2021-02-20 04:31:34 -0600

    Fixed problem in getting blockchain

--- src/main.py
+++ src/main.py
@@ -90,14 +90,26 @@ def miner_loop():
 def get_latest_blockchain():
     for peer_address in peer_addresses:
         try:
-            return jsonpickle.encode(requests.get('http://' + peer_address + ':' + str(PORT)
-                                                  + '/get_blockchain').text)
+            json_dict = json.loads(requests.get('http://' + peer_address + ':' + str(PORT) + '/get_blockchain').text.decode("utf-8"))
+            json_dict.update({"py/object":"Blockchain.Blockchain"})
+            temp_chain = jsonpickle.loads(json.dumps(json_dict))
+            return temp_chain
         except:
             continue
     return Blockchain.Blockchain()
 
 
 if __name__ == '__main__':
+
+    key_pair = RSA.generate(bits=1024)
+
+    msg1 = "abc"
+    signature = pow(int.from_bytes(hashlib.sha512(str.encode(msg1)).digest(), byteorder='big'), key_pair.d, key_pair.n)
+    a1 = Article.Article(msg1, signature, key_pair.e, key_pair.n)
+    #print(a1.verify())
+    requests.post('http://' + '34.66.161.128' + ':' + str(PORT)
+                  + '/new_article', data=jsonpickle.encode(a1))
+    '''
     peer_addresses = requests.get('http://35.225.55.196:5000/get_peer_addresses?n=16').json()
     chain = get_latest_blockchain()
 
@@ -107,6 +119,7 @@ if __name__ == '__main__':
     handler_thread.start()
     miner_thread.join()
     handler_thread.join()
+    '''
     '''
     key_pair = RSA.generate(bits=1024)
 
